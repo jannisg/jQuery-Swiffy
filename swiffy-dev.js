167e6f80d04deeb63214d40f9c3dfc84e6bd9aa6
Jannis Gundermann <jannis@gundermann.me>
2011-04-22 14:10:53 +1200

    Built compete msg() parser

--- swiffy-dev.js
+++ swiffy-dev.js
@@ -1,64 +1,97 @@
 // =================
 // = jQuery swiffy =
 // =================
-// Helpers.
-// a log function that won't break browsers if left in.
-function log( whatever ) { if (window.console && window.console.log) { window.console.log($obj); } }
-// a global function to successfully target the swf object we create (thank you Internet Explorer).
-function getSwiffy( objName ) { var isIE = $.browser.msie; return (isIE) ? window[objName] : document[objName]; }
 // start jQuery code.
 (function($){
+  // Helpers.
+  var regex = {
+    fnName : /^function\s+\(\s([\w]*)/i
+  }
+  // a log function that won't break browsers if left in.
+  window.log = function( whatever ) { 
+    if (window.console && window.console.log) { window.console.log( whatever ); } }
+  // a global function to successfully target the swf object we create (thank you Internet Explorer).
+  window.getObject = function( objName ) { var isIE = $.browser.msie; return (isIE) ? window[objName] : document[objName]; }
+
   // a function to check on a given element to be undefined or not.
-  var types = {
-    'function' : function( arg ) { console.log(arg,'this is running the "function" function'); }
-   ,'boolean'  : function( arg ) { console.log(arg,'this is running the "boolean" function'); }
-   ,'number'   : function( arg ) { console.log(arg,'this is running the "number" function'); }
-   ,'string'   : function( arg ) { console.log(arg,'this is running the "string" function'); }
-   ,'object'   : function( arg ) { console.log(arg,'this is running the "object" function'); }
-  };
-  function valid(args) {
-    if ( !args ) { 
-       return false;
+  function paramCheck( obj ) {
+
+    var callee = arguments.callee.name;
+    var caller = arguments.callee.caller.name;
+    var passed = true;
+
+    if ( typeof obj === 'object' ) { 
+      // in case we're looping over multiple tests on arguments.
+      $.each(obj, function(key, val) {
+        
+        var arg = key,
+            type = typeof val.is,
+            expected = val.expected,
+            required = ( val.required ? val.required : false);
+
+        if ( type === "undefined" && required ) {
+          log('"'+arg+'" cannot be left empty.');
+          passed = false;
+        } else if ( type !== "undefined" && type !== expected ) { 
+          log( '"'+caller+'()" triggered an exception: "'+arg+'" is expecting a "'+expected+'" but has been assigned a "'+type+'".');
+          passed = false;
+        }
+
+      });
+      
+      return passed;
+
+    } else {
+      // when we're just checking a singular argument.
+      log(callee+'() needs to be passed an object!');
     }
     
-    var state = true;
-
-    $.each(args, function(i,arg) {
-      var type = typeof arg;
-      types[type](arg);
-    });
-
-    // return state;
   }
-  
-  
-  
+    
   // A global set of functions to interact with swiffy.swf
 	window.swiffy = {
-	   init     :  function( options ) {
+	   init     :  function init( filename , count ) {
        // called when page is being loaded to initialize the plugin itself.
-       console.log(valid(arguments));
-       // if (! valid(arguments) ) return;
+       if ( !paramCheck({ // let's make sure the arguments supplied satisfy our needs.
+         
+         filename : {
+           required : true,
+           is : filename,
+           expected : 'string'
+         },
+         
+         count : {
+           required : true,
+           is : count,
+           expected : 'number'
+         }
+         
+       }) ) {
+         // in case they didn't, run this and stop the init.
+         log('init() failed, please see your log for details.');
+         return false;
+       }
+
     }
-		,ready    :  function( event ) {
+		,ready    :  function ready( fn ) {
       // called by the swiffy.swf once the clip has fully loaded and is ready to accept commands and function calls
 		}
-		,play     :  function( filename , volume ) {
+		,play     :  function play( filename , volume ) {
       // plays a specific or random song from the collection
 		}
-		,stop     :  function( filename ) {
+		,stop     :  function stop( filename ) {
       // stop a specific or all songs
 		}
-		,load     :  function( filename , path , excludeFromRandom , callback ) {
+		,load     :  function load( filename , path , excludeFromRandom , callback ) {
       // load a specific song into the collection, this clip can be excluded by the random play function and can fire a callback once loaded
 		}
-		,loop     :  function( filename , volume ) {
+		,loop     :  function loop( filename , volume ) {
       // loop a specific sound, can be a different volume than the global volume setting
 		}
-		,overlay  :  function( boolean ) {
+		,overlay  :  function overlay( boolean ) {
       // true or false. decides if sounds will play on top of eachother or triggering a second sound will stop the currently playing one first
 		}
-		,volume   :  function( volume ) {
+		,volume   :  function volume( volume ) {
       // to control the global sound volume after init (which sets it to 1 aka full volume)
 		}
 	};
